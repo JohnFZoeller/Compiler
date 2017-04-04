a1a6cc050f698f933100c6ee83b3a9cb57a06f62
John Frederick Zoeller <johnzoeller@gmail.com>
2017-04-04 14:33:11 -0700

    Add files via upload

--- Token.java
+++ Token.java
@@ -4,6 +4,18 @@ public class Token{
 	protected String input;
 
 	 public Token(String i, int r, int c){
-	 	
+	 	this.input = i;
+	 	this.row = r;
+	 	this.col = c;
+	 }
+
+	 public Token(int r, int c){
+	 	this.row = r;
+	 	this.col = c;
+	 }
+
+	 @Override
+	 public String toString(){
+	 	return "[" + row + ", " + col + "]";
 	 }
 }
