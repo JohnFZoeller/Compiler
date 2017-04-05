0029b1e4d5bef8da338b6f37e79c3a24832bde33
Destiny Boyer <destiny@destinyboyer.com>
2017-04-05 11:20:55 -0700

    Added keywordmap, changed comments in operatormap

--- Tokens/OperatorMap.java
+++ Tokens/OperatorMap.java
@@ -1,11 +1,11 @@
 /**
  * 
- * TokenName is a utility class that contains all of the Operator
+ * OperatorMap is a utility class that contains all of the Operator
  * tokens specific to a given language. Operator tokens are stored
  * in a HashMap. The key for each key-value pair is the operator
  * itself, and the value is a description of the operator that can
  * be printed for additional information. Note that the HashMap
- * does not allow duplicate keys.
+ * does not allow duplicate keys. Class is final.
  *
  * @author Destiny Boyer
  * @author John Zoeller
